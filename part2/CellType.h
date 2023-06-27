03f7580c00e104138eaa4b78b3c20b059cdf527e
amirb <amirbourvine@campus.technion.ac.il>
2023-06-28 00:34:17 +0300

    CellType.h written

---
+++ part2/CellType.h
@@ -0,0 +1,10 @@
+//
+// Created by amirb on 28/06/2023.
+//
+
+#ifndef OOP5_CELLTYPE_H
+#define OOP5_CELLTYPE_H
+
+enum CellType {EMPTY, X, A, B, C, D, E, F, G, H, I, J, K, O, P, Q, R};
+
+#endif //OOP5_CELLTYPE_H
